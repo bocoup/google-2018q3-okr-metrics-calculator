f8fc051cbaaa3c2bbed3a13b8549314d9f38bb9c
Mike Pennisi <mike@mikepennisi.com>
2018-09-19 16:10:51 -0400

    Extend interface to allow multiple commands

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ const path = require('path');
 
 const octokit = require('@octokit/rest')()
 const Replay = require('replay');
-
-const measureWptPrBot = require('./measure-wpt-pr-bot');
+const yargs = require('yargs');
 
 const token = require('./util/load-gh-token')(
   path.join(__dirname, '..', 'github-token.txt')
@@ -20,4 +19,10 @@ Replay.headers = Replay.headers
 
 octokit.authenticate({ type: 'token', token });
 
-measureWptPrBot('2018-07-01', '2018-10-01');
+yargs.command('wpt-pr-bot', 'score the bot', () => {
+    const measureWptPrBot = require('./measure-wpt-pr-bot');
+    measureWptPrBot('2018-07-01', '2018-10-01');
+  })
+  .demandCommand()
+  .strict()
+  .argv;
